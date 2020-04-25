5a47cd3e5fec5e49982230e0e44fa77d6450b9d7
KWL <k-airus-kwl@outlook.com>
2020-04-25 13:20:52 -0700

    [KWSE/Framework/Math] - Added Functions for Vector2 and Vector3 .

--- Framework/Math/Inc/EngineMath.h
+++ Framework/Math/Inc/EngineMath.h
@@ -19,6 +19,7 @@ namespace KWSE::Math
 	const Vector2 Vector2::Zero{0.0f};
 	const Vector2 Vector2::One{1.0f };
 
+
 	//Vector3
 	const Vector3 Vector3::XAxis{ 1.0f,0.0f ,0.0f};
 	const Vector3 Vector3::YAxis{ 0.0f,1.0f,0.0f };
@@ -27,14 +28,143 @@ namespace KWSE::Math
 	const Vector3 Vector3::Zero{ 0.0f };
 	const Vector3 Vector3::One{ 1.0f };
 
-	//Matrix3
+	//Matrix
 	const Matrix3 Matrix3::Identity
 	{
 		1.0f , 0.0f , 0.0f,
 		0.0f , 1.0f , 0.0f,
 		0.0f , 0.0f , 1.0f
 	};
+	const Matrix4 Matrix4::Identity
+	{
+		1.0f, 0.0f, 0.0f, 0.0f,
+		0.0f, 1.0f, 0.0f, 0.0f,
+		0.0f, 0.0f, 1.0f, 0.0f,
+		0.0f, 0.0f, 0.0f, 1.0f,
+	};
+	
+	//
+	// Dot 
+	constexpr float Dot(const Vector2& v0, const Vector2& v1)
+	{
+		return (v0.x * v1.x) + (v0.y * v1.y);
+	}
+	constexpr float Dot(const Vector3& v0, const Vector3& v1)
+	{
+		return (v0.x * v1.x) + (v0.y * v1.y) + (v0.z * v1.z);
+	}
 
+	// MagnitudeSqr
+	constexpr float MagnitudeSqr(const Vector2& v)
+	{
+		return Dot(v, v);
+	}
+	constexpr float MagnitudeSqr(const Vector3& v)
+	{
+		return (v.x*v.x) + (v.y*v.y) + (v.z*v.z);
+	}
+
+	// Magnitude
+	inline float Magnitude(const Vector2& v)
+	{
+		return sqrt(MagnitudeSqr(v));
+	}
+	inline float Magnitude(const Vector3& v)
+	{
+		return sqrt(MagnitudeSqr(v));
+	}
+
+	// Normalize
+	inline Vector2 Normalize(const Vector2& v)
+	{
+		return v / Magnitude(v);
+	}
+	inline Vector3 Normalize(const Vector3& v)
+	{
+		ASSERT(Magnitude(v) != 0," Tring to / length , length is 0 !!! ");
+		return v / Magnitude(v);
+	}
+	
+	// DistanceSqr
+	constexpr float DistanceSqr(const Vector3& v0, const Vector3 & v1)
+	{
+		float x = v0.x - v1.x;
+		float y = v0.y - v1.y;
+		float z = v0.z - v1.z;
+
+		return (x*x + y * y + z * z);
+	}
+	// Distance
+	inline float Distance(const Vector3& v0, const Vector3 &v1)
+	{
+		float x = v0.x - v1.x;
+		float y = v0.y - v1.y;
+		float z = v0.z - v1.z;
+
+		return sqrtf(x*x + y * y + z * z);
+	}
+
+	// Cross
+	constexpr Vector3 Cross(const Vector3& u, const Vector3& v)
+	{
+		return  Vector3((u.y*v.z - u.z*v.y),
+						(u.z*v.x - u.x*v.z),
+						(u.x*v.y - u.y*v.x));
+	}
+
+	// Transform
+	constexpr Vector3 TransformCoord(const Vector3& v, const Matrix4& m)
+	{
+		return {
+			(v.x * m._11) + (v.y * m._21) + (v.z * m._31) + (1.0f * m._41),
+			(v.x * m._12) + (v.y * m._22) + (v.z * m._32) + (1.0f * m._42),
+			(v.x * m._13) + (v.y * m._23) + (v.z * m._33) + (1.0f * m._43)
+		};
+	}
+	constexpr Vector3 TransformNormal(const Vector3& v, const Matrix4& m)
+	{
+		return {
+			(v.x * m._11) + (v.y * m._21) + (v.z * m._31),
+			(v.x * m._12) + (v.y * m._22) + (v.z * m._32),
+			(v.x * m._13) + (v.y * m._23) + (v.z * m._33)
+		};
+	}
+
+	// GetRight
+	constexpr Vector3 GetRight(const Matrix4& m)
+	{
+		return { m._11, m._12, m._13 };
+	}
+
+	// GetUp
+	constexpr Vector3 GetUp(const Matrix4& m)
+	{
+		return { m._21, m._22, m._23 };
+	}
+
+	// GetLook
+	constexpr Vector3 GetLook(const Matrix4& m)
+	{
+		return { m._31, m._32, m._33 };
+	}
+
+	// GetTranslation
+	constexpr Vector3 GetTranslation(const Matrix4& m)
+	{
+		return { m._41, m._42, m._43 };
+	}
+
+	// Lerp
+	constexpr float Lerp(float a, float b, float t)
+	{
+		return a + ((b - a) * t);
+	}
+	constexpr Vector3 Lerp(const Vector3& v0, const Vector3& v1, float t)
+	{
+		return v0 + ((v1 - v0) * t);
+	}
+
+	// Transpose
 	constexpr Matrix3 Transpose(const Matrix3& m)
 	{
 		return
@@ -45,6 +175,7 @@ namespace KWSE::Math
 		};
 	}
 
+	// Determinant
 	constexpr float Determinant(const Matrix3& m)
 	{
 		//11a, 12b, 13c
@@ -59,6 +190,7 @@ namespace KWSE::Math
 		}
 			;
 	}
+	// Adjoint
 	constexpr Matrix3 Adjoint(const Matrix3& m)
 	{
 		return Matrix3
@@ -76,6 +208,7 @@ namespace KWSE::Math
 			+((m._11*m._22)-(m._12*m._21))
 		};
 	}
+	// Inverse
 	constexpr Matrix3 Inverse(const Matrix3& m)
 	{
 		const float determinant = Determinant(m);
@@ -83,14 +216,7 @@ namespace KWSE::Math
 		return Adjoint(m) *inverseDeterminant;
 	}
 	
-	//Matrix4
-	const Matrix4 Matrix4::Identity
-	{
-		1.0f, 0.0f, 0.0f, 0.0f,
-		0.0f, 1.0f, 0.0f, 0.0f,
-		0.0f, 0.0f, 1.0f, 0.0f,
-		0.0f, 0.0f, 0.0f, 1.0f,
-	};
+	// RotationAxis
 	Matrix4 Matrix4::RotationAxis(const Vector3& axis, float radian)
 	{
 		const Vector3 u = Normalize(axis);
@@ -121,49 +247,10 @@ namespace KWSE::Math
 	}
 
 
-	constexpr float Dot(const Vector3& v0, const Vector3& v1)
-	{
-		return (v0.x * v1.x) + (v0.y * v1.y) + (v0.z * v1.z);
-	}
-	constexpr float MagnitudeSqr(const Vector3& v)
-	{
-		return (v.x*v.x) + (v.y*v.y) + (v.z*v.z);
-	}
 
 	//Inline
 	//Constexpr
 
-	inline float Magnitude(const Vector3& v)
-	{
-		return sqrtf(MagnitudeSqr(v));
-	}
-	constexpr float DistanceSqr(const Vector3& v0, const Vector3 & v1)
-	{
-		float x = v0.x - v1.x;
-		float y = v0.y - v1.y;
-		float z = v0.z - v1.z;
 
-		return (x*x + y * y + z * z);
-	}
-	inline float Distance(const Vector3& v0, const Vector3 &v1)
-	{
-		float x = v0.x - v1.x;
-		float y = v0.y - v1.y;
-		float z = v0.z - v1.z;
-
-		return sqrtf(x*x + y * y + z * z);
-	}
-	inline Vector3 Normalize(const Vector3& v)
-	{
-		float length = Magnitude(v);
-		ASSERT(length != 0," Tring to / length , length is 0 !!! ");
-		return { Vector3(v.x / length, v.y / length, v.z / length) };
-	}
-	constexpr Vector3 Cross(const Vector3& u, const Vector3& v)
-	{
-		return  Vector3((u.y*v.z - u.z*v.y),
-						(u.z*v.x - u.x*v.z),
-						(u.x*v.y - u.y*v.x));
-	}
 
 }
